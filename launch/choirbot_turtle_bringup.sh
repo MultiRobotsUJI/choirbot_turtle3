6198ed6f1ab2f5c27a2fbc7b4807d5c7bf8bc334
AbdelrhmanBassiouny <abdelrhman_bassuny@hotmail.com>
2022-11-17 14:05:34 +0000

    forgot .py

--- launch/choirbot_turtle_bringup.sh
+++ launch/choirbot_turtle_bringup.sh
@@ -4,4 +4,4 @@ export ROS_DOMAIN_ID=30
 source /opt/ros/foxy/setup.bash
 
 export TURTLEBOT3_MODEL=burger
-ros2 launch choirbot_turtle3 new_tb3_bringup.launch
+ros2 launch choirbot_turtle3 new_tb3_bringup.launch.py
